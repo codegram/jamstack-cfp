04b1a4bf05f3e7f0e7eda68fd82eb01f364bca30
Nuria <nuria@codegram.com>
2020-04-21 15:13:10 +0200

    add handle submission workflow

---
+++ actions/process-input.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const { client_payload } = require(process.env.GITHUB_EVENT_PATH);
+
+// Thanks @hagemann https://gist.github.com/hagemann/382adfc57adbd5af078dc93feef01fe1
+function slugify(string) {
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------";
+  const p = new RegExp(a.split("").join("|"), "g");
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+}
+
+const fileName = `${slugify(client_payload.name)}-${slugify(
+  client_payload.title
+)}`;
+
+fs.writeFileSync(
+  `submissions/${fileName}.json`,
+  JSON.stringify(client_payload)
+);
